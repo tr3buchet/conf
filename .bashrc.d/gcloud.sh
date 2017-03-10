4ea44b854b3dd97523bb7af74af1dd4070889b4b
Trey Morris <treyemorris@gmail.com>
2017-03-10 13:45:46 -0600

    added gcloud.sh to .bashrc.d files
    gcloud bussiness will be conditionally sourced
    if it exists

---
+++ .bashrc.d/gcloud.sh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/trey/google-cloud-sdk/path.bash.inc' ]; then source '/home/trey/google-cloud-sdk/path.bash.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/home/trey/google-cloud-sdk/completion.bash.inc' ]; then source '/home/trey/google-cloud-sdk/completion.bash.inc'; fi
